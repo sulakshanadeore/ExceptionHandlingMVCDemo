49768f7fcd3f3bd8358489ff78cf5fee9bb03f80
sulakshanadeore <deoresulakshana@gmail.com>
2025-03-05 14:23:21 +0530

    Caching

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExceptionHandlingMVCDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        //server is the default location for the cache
+        //[OutputCache(Duration =10,Location =System.Web.UI.OutputCacheLocation.Server,VaryByParam ="username")]
+     //   [OutputCache(CacheProfile = "profile10")]
+        [OutputCache(Duration =int.MaxValue,VaryByParam ="username")]
+        public ActionResult Index(string username)
+        {
+            DateTime dt=GetDateandTime();
+            string s = dt.ToLongTimeString();
+            ViewBag.datetimeShow = "Hello " + username + " " + s;
+            return View();
+        }
+
+        [ChildActionOnly]
+        public DateTime GetDateandTime()
+        {
+            DateTime s = DateTime.Now;
+            return s;
+        }
+
+
+        //public ActionResult Index()
+        //{
+        //        string s=DateTime.Now.ToLongTimeString();
+        //    ViewBag.datetimeShow = s;
+        //    return View();
+        //}
+
+    }
+}
